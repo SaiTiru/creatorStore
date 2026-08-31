6e5aaece6446c9aedb52f81725d141ba97a7f8f2
Sai <vsai.tirupati@gmail.com>
2026-08-30 22:34:02 -0400

    chore : add env data for application.yaml

--- src/main/java/com/stiru/creatorstore/CreatorStoreApplication.java
+++ src/main/java/com/stiru/creatorstore/CreatorStoreApplication.java
@@ -1,5 +1,7 @@
 package com.stiru.creatorstore;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CreatorStoreApplication {
 
     public static void main(String[] args) {
+        // Configuration of dotenv-java
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach((DotenvEntry entry) -> System.setProperty(
+                entry.getKey(),entry.getValue()
+        ));
+
         SpringApplication.run(CreatorStoreApplication.class, args);
     }
 
