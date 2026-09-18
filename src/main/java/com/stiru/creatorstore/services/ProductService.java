9fc549adb2a92963b415c320af02b6db920f9472
Sai <vsai.tirupati@gmail.com>
2026-09-17 20:45:49 -0400

    db pw changed

--- src/main/java/com/stiru/creatorstore/services/ProductService.java
+++ src/main/java/com/stiru/creatorstore/services/ProductService.java
@@ -2,7 +2,6 @@ package com.stiru.creatorstore.services;
 
 import com.stiru.creatorstore.entites.Product;
 import com.stiru.creatorstore.repositories.ProductRepository;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
